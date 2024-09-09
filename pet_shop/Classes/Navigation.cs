3026e75925b08024d64e80cdf5720645847b07a2
lisav <lisav@DJ-PC-K207-08>
2024-09-09 16:35:17 +0300

    Добавьте файлы проекта.

---
+++ pet_shop/Classes/Navigation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace pet_shop.Classes
+{
+    public static class Navigation
+    {
+        public static Frame ActiveFrame { get; set; }
+    }
+}
