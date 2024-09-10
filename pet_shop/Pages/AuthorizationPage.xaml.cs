0967d992f573963840f660af9c365105e1ea530b
lisav <lisav@DJ-PC-K207-08>
2024-09-10 12:04:06 +0300

    Added Login Logic

--- pet_shop/Pages/AuthorizationPage.xaml.cs
+++ pet_shop/Pages/AuthorizationPage.xaml.cs
@@ -27,7 +27,57 @@ namespace pet_shop.Pages
 
         private void AuthButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                string Login = LoginTextBox.Text.ToString().Trim();
+                string Password = PasswordBox.Password.ToString().Trim();
+                if (string.IsNullOrEmpty(Login))
+                {
+                    errors.AppendLine("Введите логин");
+                }
+                if (string.IsNullOrEmpty(Password))
+                {
+                    errors.AppendLine("Введите пароль");
+                }
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                else
+                {
+                    if (Models.pets_shopEntities.GetContext().User.Any(d => d.UserLogin == Login 
+                    && d.UserPassword == Password))
+                    {
+                        var role = Models.pets_shopEntities.GetContext().User.Where(d => d.UserLogin == Login
+                    && d.UserPassword == Password).FirstOrDefault().Role.RoleName;
+                        switch (role.ToString())
+                        {
+                            case "Администратор":
+                                Classes.Navigation.ActiveFrame.Navigate(new NewProductList());
+                                MessageBox.Show("Успех!","Успех!",MessageBoxButton.OK, MessageBoxImage.Information);
+                                break;
+                            case "Клиент":
+                                Classes.Navigation.ActiveFrame.Navigate(new NewProductList());
+                                MessageBox.Show("Успех!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                                break;
+                            case "Менеджер":
+                                Classes.Navigation.ActiveFrame.Navigate(new NewProductList());
+                                MessageBox.Show("Успех!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неправильный логин/пароль", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void GuestButton_Click(object sender, RoutedEventArgs e)
